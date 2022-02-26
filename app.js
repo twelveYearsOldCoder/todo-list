f1a1eedc24d557882a0f6772bb2c1edf4301ddda
nikos kon <niko.ss@windowslive.com>
2022-02-26 16:31:32 +0200

    added style and functionality

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const { urlencoded } = require("express");
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+var listItem = [];
+var workListItem = [];
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+/*
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var currentDay = new Date().getDay();
 var day = weekday[currentDay];
+*/
 
-app.get("/", (req, res)=>{
-//key: value
-//you can add more than one, this is a js object
-res.render('list', {day: day});
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today = new Date().toLocaleDateString("en-US", options);
+
+//console.log(today.toLocaleDateString("en-US", options));
+
+
+app.get("/", (req, res) => {
+    //key: value
+    //you can add more than one, this is a js object
+    res.render('list', { day: today, listItem: listItem, listTitle:"Grocery" });
 });
-app.listen(3000, ()=>{
+
+app.get("/work", (req, res) => {
+    //key: value
+    //you can add more than one, this is a js object
+    res.render('list', { day: today, listItem: workListItem, listTitle:"Work" });
+});
+
+
+app.post("/", (req, res) => {
+    if(req.body.list==="Work"){
+        workListItem.push(req.body.todoEntry);
+        res.redirect("/work");
+
+    }else if (req.body.list==="Grocery"){
+        listItem.push(req.body.todoEntry);
+        res.redirect("/");
+
+    }
+
+});
+app.listen(3000, () => {
     console.log("server started at  port 3000");
 })
