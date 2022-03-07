6bb2502819c354585aaa1e2804010bf99a44b60a
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-03-07 19:46:54 +0200

    added the functionality to create custom lists via new urls

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { urlencoded } = require("express");
 //επειδη εχει οριστει date, καθε φορα θα πρεπει να χρισιμοποιησω την date() Και οχι το ονομα που εχει οριστει στο Module
-const date = require(__dirname+"/date.js");
+const date = require(__dirname + "/date.js");
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -13,60 +13,84 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 //create schema
-const listSchema ={
+const itemSchema = {
     name: String
 };
 //create the model
-const Item = mongoose.model("Item", listSchema);
+const Item = mongoose.model("Item", itemSchema);
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+const List = mongoose.model("List", listSchema);
 
 const item1 = new Item({
     name: "First"
 });
 const item2 = new Item({
     name: "Second"
-});const item3 = new Item({
+}); const item3 = new Item({
     name: "Third"
 });
 
-const itemList = [item1, item2, item3];
-var workListItem = [];
+const defaultItems = [item1, item2, item3];
 
-let today= date.getDate();
+let today = date.getDate();
 
 app.get("/", (req, res) => {
-    Item.find({},(err, results)=>{
+    Item.find({}, (err, results) => {
         //only populate the default array the first time
-        if(results.length===0){
-            Item.insertMany(itemList, (err)=>{
-                if (err){
+        if (results.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
                     console.log(err);
-                }else {
+                } else {
                     console.log("the default array was inserted successfully");
                 }
             });
             res.redirect("/");
-        }else{
-            res.render('list', { day: "Today", listItem: results, listTitle:"Grocery" });
+        } else {
+            res.render('list', { day: "Today", listItem: results, listTitle: "Home" });
 
         }
 
     })
 
-    //key: value
-    //you can add more than one, this is a js object
-    //changed  day: today -> day: "today" in order to remove some complexity
+
 });
 
-app.get("/work", (req, res) => {
-    //key: value
-    //you can add more than one, this is a js object
-    res.render('list', { day: today, listItem: workListItem, listTitle:"Work" });
+//dynamicly create pages
+app.get("/:catName", (req, res) => {
+    const reqUrl = req.params.catName;
+    List.findOne({ name: reqUrl }, (err, result) => {
+        console.log(reqUrl);
+        if (!err) {
+            console.log("not error");
+            if (!result) {
+                console.log("no result");
+                const list = new List({
+                    name: reqUrl,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/"+reqUrl);
+            } else {
+                console.log("entered else");
+                //   res.render("list", { day: "Today", listItem: result, listTitle:"Grocery" })
+                res.render("list", { day: "Today", listItem: result.items, listTitle: result.name })
+            }
+        }
+    })
+
 });
+
+
 
 
 app.post("/", (req, res) => {
-const itemName = req.body.todoEntry;
-    const newItem = new Item ({
+    const itemName = req.body.todoEntry;
+    const newItem = new Item({
         name: itemName
     })
     newItem.save();
@@ -75,18 +99,20 @@ const itemName = req.body.todoEntry;
 
 
 app.post("/delete", (req, res) => {
-const itemID =req.body.checkbox;
-    Item.deleteOne({itemID}, (err)=>{
-        if(err){
+    const itemID = req.body.checkbox;
+    Item.deleteOne({ itemID }, (err) => {
+        if (err) {
             console.log(err);
         }
-        else{
-            console.log("successfully deleted the item with id "+itemID);
+        else {
+            console.log("successfully deleted the item with id " + itemID);
         }
     })
-       res.redirect("/");
-    });
+    res.redirect("/");
+});
+
+
 
 app.listen(3000, () => {
     console.log("server started at  port 3000");
-})
+});
